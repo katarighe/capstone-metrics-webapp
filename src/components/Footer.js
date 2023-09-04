b6a62befb9822fbb885dca1e5956c559a1f6d8b0
katarighe <m.ighe@hotmail.com>
2023-09-04 18:29:47 +0300

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div>
+    <h3 className="footer">A Microverse Capstone Project by Mohamed Aden Ighe</h3>
+  </div>
+);
+
+export default Footer;
