81110782abbad5c80df28649f0103c9700161686
katarighe <m.ighe@hotmail.com>
2023-09-04 19:05:22 +0300

    Create structure for the Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { search } from '../redux/Home/homeSlice';
+import searchLogo from '../assets/icons/magnifying-glass-solid.svg';
 
 const Search = () => {
   const dispatch = useDispatch();
   const onSearchHandler = (e) => {
     dispatch(search(e.target.value));
   };
+
   return (
-    <div className="search_box">
-      {/* <img className="" src={} alt="" /> */}
+    <div className="search_container">
+      <img className="icon" src={searchLogo} alt="" />
       <input
         type="text"
         placeholder="Search"
-        className="search_input"
+        className="input"
         onChange={onSearchHandler}
       />
     </div>
