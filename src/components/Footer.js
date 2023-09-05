557d420a3fdd99f014c0b2600f6640ae5f54adfc
katarighe <m.ighe@hotmail.com>
2023-09-05 15:00:04 +0300

    Fix the footer title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
   <div>
-    <h3 className="footer">A Microverse Capstone Project by Mohamed Aden Ighe</h3>
+    <h3 className="footer">A Microverse Capstone Project by Mohamed A. Ighe</h3>
   </div>
 );
 
