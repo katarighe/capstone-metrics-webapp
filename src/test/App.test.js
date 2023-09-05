00e2f5e534f4e4fab9d9bee3fdb15730f306383e
katarighe <m.ighe@hotmail.com>
2023-09-05 19:06:18 +0300

    Remove unwanted line

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from '../App';
 import store from '../redux/store';
