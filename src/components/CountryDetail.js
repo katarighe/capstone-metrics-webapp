db2ebd242c4e7541fe736213cb76fa18ffb5510e
katarighe <m.ighe@hotmail.com>
2023-09-05 12:44:55 +0300

    Link the CSS design file and fixing the structure

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -3,35 +3,33 @@ import { useSelector } from 'react-redux';
 import rightArrow from '../assets/icons/circle-arrow-right-solid.svg';
 import Footer from './Footer';
 import NavDetail from './NavDetail';
+import './CountryDetail.css';
 
 const CountryDetail = () => {
   const country = useSelector((state) => state.countryDetail.detailCountry);
   return (
-    <div className="detail_body">
+    <div className="country_detail_body">
       <NavDetail />
       {country.length > 0 ? (
         <div>
-          <div className="detail__header__box">
-            <div className="detail__header__box__map">
+          <div className="country_detail_header_box">
+            <div className="country_detail_header_box_map">
               <img src={country[0].map} alt="" />
             </div>
-            <div className="detail__header__box__text">
+            <div className="country_detail_header_box_text">
               <h1>{country[0].name}</h1>
-              <h2>Official Name</h2>
               <p>
+                {' '}
+                (
                 {country.official}
                 {' '}
-              </p>
-              <h2>Current Population</h2>
-              <p>
-                {country[0].population}
-                {' '}
+                )
               </p>
             </div>
           </div>
-          <h2 className="country__breakdown__text">Country Breakdown</h2>
-          <div className="detail__breakdown__box">
-            <div className="detail__breakdown__box__text">
+          <h2 className="country_overview_headline">Country Overview</h2>
+          <div className="country_detail_overview_box">
+            <div className="country_detail_overview_box_text color_blue">
               <h3>Area</h3>
               <p>
                 {country[0].area}
@@ -42,7 +40,17 @@ const CountryDetail = () => {
                 <img src={rightArrow} alt="Right Arrow" />
               </button>
             </div>
-            <div className="detail__breakdown__box__text color__dark">
+            <div className="country_detail_overview_box_text">
+              <h3>Population</h3>
+              <p>
+                {country[0].population}
+                {' '}
+              </p>
+              <button type="button">
+                <img src={rightArrow} alt="Right Arrow" />
+              </button>
+            </div>
+            <div className="country_detail_overview_box_text color_blue">
               <h3>Country Flag</h3>
               <img
                 className="country_flag"
@@ -53,28 +61,28 @@ const CountryDetail = () => {
                 <img src={rightArrow} alt="Right Arrow" />
               </button>
             </div>
-            <div className="detail__breakdown__box__text">
+            <div className="country_detail_overview_box_text">
               <h3>Capital</h3>
               <p>{country.capital}</p>
               <button type="button">
                 <img src={rightArrow} alt="Right Arrow" />
               </button>
             </div>
-            <div className="detail__breakdown__box__text color__dark">
+            <div className="country_detail_overview_box_text color_blue">
               <h3>Continent</h3>
               <p>{country.continents}</p>
               <button type="button">
                 <img src={rightArrow} alt="Right Arrow" />
               </button>
             </div>
-            <div className="detail__breakdown__box__text">
+            <div className="country_detail_overview_box_text">
               <h3>Timezone</h3>
               <p>{country[0].timezones}</p>
               <button type="button">
                 <img src={rightArrow} alt="Right Arrow" />
               </button>
             </div>
-            <div className="detail__breakdown__box__text color__dark">
+            <div className="country_detail_overview_box_text color_blue">
               <h3>Languages</h3>
               <p>{country.languages}</p>
               <button type="button">
