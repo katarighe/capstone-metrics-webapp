406a7d514358628b8e8b2b70ad037254769af62f
katarighe <m.ighe@hotmail.com>
2023-09-05 11:40:42 +0300

    Link the CSS file, as well as fixing the overall design structure

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { search } from '../redux/Home/homeSlice';
 import searchLogo from '../assets/icons/magnifying-glass-solid.svg';
+import './Search.css';
 
 const Search = () => {
   const dispatch = useDispatch();
   const onSearchHandler = (e) => {
     dispatch(search(e.target.value));
   };
-
   return (
     <div className="search_container">
-      <img className="icon" src={searchLogo} alt="" />
+      <img className="search_icon" src={searchLogo} alt="" />
       <input
         type="text"
         placeholder="Search"
-        className="input"
+        className="search_input"
         onChange={onSearchHandler}
       />
     </div>
